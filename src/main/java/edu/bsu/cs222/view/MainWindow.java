dcd66de39e20cbade2f13c982ffdb6cac347772a
Pirate <Timnich2001@Gmail.com>
2022-02-23 11:59:41 -0500

    Sets up the PokeAPI and JavaFX

---
+++ src/main/java/edu/bsu/cs222/view/MainWindow.java
@@ -0,0 +1,12 @@
+package edu.bsu.cs222.view;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainWindow extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.show();
+    }
+}
