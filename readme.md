5bb244830c8528e95efb5843583d604f967e42d0
Pirate <Timnich2001@Gmail.com>
2022-03-01 15:27:12 -0500

    Add search bar to main window

--- readme.md
+++ readme.md
@@ -1,6 +1,5 @@
-Created by:
-Tim Nicholas (tsnicholas@bsu.edu)
-Kameron Rogers (karogers3@bsu.edu)
-Paul Lian (nplian@bsu.edu)
-Kuljit Mann (insert email here)
-Anthony Miller (acmiller@bsu.edu)
+Created by: Tim Nicholas (tsnicholas@bsu.edu), 
+Kameron Rogers (karogers3@bsu.edu), 
+Paul Lian (nplian@bsu.edu), 
+Kuljit Mann (insert email here),
+and Anthony Miller (acmiller@bsu.edu)
--- src/main/java/edu/bsu/cs222/view/MainWindow.java
+++ src/main/java/edu/bsu/cs222/view/MainWindow.java
@@ -1,12 +1,53 @@
 package edu.bsu.cs222.view;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class MainWindow extends Application {
+    private final TextField searchInput = new TextField();
+    private final Button searchButton = new Button("Search");
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) {
+        setUpWindowBasics(primaryStage);
+        setUpExit(primaryStage);
         primaryStage.show();
+    }
+
+    private void setUpWindowBasics(Stage primaryStage) {
+        primaryStage.setTitle("Pokedex");
+        primaryStage.setScene(new Scene(createMainWindow()));
+    }
+
+    private void setUpExit(Stage primaryStage) {
+        primaryStage.setOnCloseRequest(X -> {
+            Platform.exit();
+            System.exit(0);
+        });
+    }
+
+    private Parent createMainWindow() {
+        VBox mainWindow = new VBox();
+        mainWindow.getChildren().addAll(
+                createSearchBar()
+        );
+
+        return mainWindow;
+    }
+
+    private Parent createSearchBar() {
+        HBox searchBar = new HBox();
+        searchBar.getChildren().addAll(
+                searchInput,
+                searchButton
+        );
+        return searchBar;
     }
 }
