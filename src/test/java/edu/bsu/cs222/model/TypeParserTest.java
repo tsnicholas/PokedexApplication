a478f34569f62f922f3f08ceef584c2ba2d0be14
Kameron Rogers <karogers3@bsu.edu>
2022-03-15 22:31:42 -0400

    Add more parses to TypeParser and resource files for types.
    TypeParser can now parse for the types which each type is weak, resistant,
    and immune to. Each new method has been tested.

--- src/test/java/edu/bsu/cs222/model/TypeParserTest.java
+++ src/test/java/edu/bsu/cs222/model/TypeParserTest.java
@@ -10,10 +10,13 @@ import java.util.Scanner;
 
 class TypeParserTest {
     private final TypeParser typeParser = new TypeParser();
-    private final String pikachuData = resourceToString("pikachu.json");
-    private final String clefableData = resourceToString("clefable.json");
-    private final String charizardData = resourceToString("charizard.json");
+    private final Object clefableDocument = typeParser.parseJson(resourceToString("clefable.json"));
+    private final Object charizardDocument = typeParser.parseJson(resourceToString("charizard.json"));
+    private final Object electricDocument = typeParser.parseJson(resourceToString("electric.json"));
+    private final Object bugDocument = typeParser.parseJson(resourceToString("bug.json"));
+    private final Object ghostDocument = typeParser.parseJson(resourceToString("ghost.json"));
     private List<String> types = new ArrayList<>();
+    private String jsonPathRoot = "";
 
     private String resourceToString(String fileName) {
         InputStream testStreamData = Thread.currentThread().getContextClassLoader().getResourceAsStream(fileName);
@@ -23,21 +26,55 @@ class TypeParserTest {
     }
 
     @Test
-    public void testParseForType(){
-        types = typeParser.parseForType(pikachuData);
-        Assertions.assertEquals("electric", types.get(0));
-    }
-
-    @Test
     public void testParseForPastType() {
-        types = typeParser.parseForType(clefableData);
+        types = typeParser.parseForTypes(clefableDocument);
         Assertions.assertEquals("normal", types.get(0));
     }
 
     @Test
-    public void testParseForMultipleTypes() {
-        types = typeParser.parseForType(charizardData);
+    public void testParseForTypes() {
+        types = typeParser.parseForTypes(charizardDocument);
         Assertions.assertEquals("fire", types.get(0));
         Assertions.assertEquals("flying", types.get(1));
     }
+
+    @Test
+    public void testRootOfDamageRelations() {
+        jsonPathRoot = typeParser.getRootOfDamageRelations(electricDocument);
+        Assertions.assertEquals("$.damage_relations", jsonPathRoot);
+    }
+
+    @Test
+    public void testRootOfPastDamageRelations() {
+        jsonPathRoot = typeParser.getRootOfDamageRelations(bugDocument);
+        Assertions.assertEquals("$.past_damage_relations[0].damage_relations", jsonPathRoot);
+    }
+
+    @Test
+    public void testParseWeakTo() {
+        jsonPathRoot = typeParser.getRootOfDamageRelations(bugDocument);
+        types = typeParser.parseWeakTo(bugDocument, jsonPathRoot);
+        Assertions.assertEquals("flying", types.get(0));
+        Assertions.assertEquals("rock", types.get(1));
+        Assertions.assertEquals("fire", types.get(2));
+        Assertions.assertEquals("poison", types.get(3));
+    }
+
+    @Test
+    public void testParseResistantTo() {
+        jsonPathRoot = typeParser.getRootOfDamageRelations(electricDocument);
+        types = typeParser.parseResistantTo(electricDocument, jsonPathRoot);
+        Assertions.assertEquals("flying", types.get(0));
+        Assertions.assertEquals("steel", types.get(1));
+        Assertions.assertEquals("electric", types.get(2));
+    }
+
+    @Test
+    public void testParseImmuneTo() {
+        jsonPathRoot = typeParser.getRootOfDamageRelations(ghostDocument);
+        types = typeParser.parseImmuneTo(ghostDocument, jsonPathRoot);
+        Assertions.assertEquals("normal", types.get(0));
+        Assertions.assertEquals("fighting", types.get(1));
+    }
+
 }
