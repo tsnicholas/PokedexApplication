75d944f2390c190952e100ecfc658ab236c52c45
Pirate <Timnich2001@Gmail.com>
2022-03-19 19:39:38 -0400

    Adds a URLProcessor

--- src/main/java/edu/bsu/cs222/model/URLProcessor.java
+++ src/main/java/edu/bsu/cs222/model/URLProcessor.java
@@ -3,36 +3,41 @@ package edu.bsu.cs222.model;
 import edu.bsu.cs222.view.ErrorWindow;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
 import java.nio.charset.Charset;
 
-public class tempURLProcessor {
-    private final InputStreamConverter inputStreamConverter = new InputStreamConverter();
+public class URLProcessor {
 
-    public tempURLProcessor() {}
+    public URLProcessor() {}
 
-    public Object process(String pokemon) {
+    public URL getPokemonURL(String pokemon) {
         try {
             String encodedPokemon = URLEncoder.encode(pokemon, Charset.defaultCharset());
             String urlString = String.format("https://pokeapi.co/api/v2/Pokemon/%s", encodedPokemon);
-            URLConnection urlConnection = connectToURL(urlString);
-            return inputStreamConverter.inputStreamToJsonObject(urlConnection.getInputStream());
-        }
-        catch(IOException ioException) {
-            throw new IllegalStateException();
-        }
-    }
-
-    private URLConnection connectToURL(String urlString) {
-        try {
-            URL url = new URL(urlString);
-            return url.openConnection();
+            return new URL(urlString);
         }
         catch(MalformedURLException malformedURLException) {
             throw new IllegalStateException(malformedURLException);
+        }
+    }
+
+    public InputStream getInputStream(URL url) {
+        try {
+            URLConnection urlConnection = openURLConnection(url);
+            return urlConnection.getInputStream();
+        }
+        catch(IOException ioException) {
+            throw new IllegalStateException(ioException);
+        }
+    }
+
+    private URLConnection openURLConnection(URL url) {
+        try {
+            return url.openConnection();
         }
         catch(IOException networkError) {
             ErrorWindow networkErrorWindow = new ErrorWindow("A network error has occurred");
