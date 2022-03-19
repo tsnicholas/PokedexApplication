2ef41e3ca5b5ddb459ce52910495099b640692cb
Kameron Rogers <karogers3@bsu.edu>
2022-03-19 16:52:38 -0400

    Remove levelLearnedList from Move as it was redundant

--- src/main/java/edu/bsu/cs222/model/Move.java
+++ src/main/java/edu/bsu/cs222/model/Move.java
@@ -2,8 +2,8 @@ package edu.bsu.cs222.model;
 
 import java.util.List;
 
-public record Move(String name, String type, String PP, String power, String accuracy, List<String> learnMethod, List<Integer> levelLearned) {
+public record Move(String name, String type, String PP, String power, String accuracy, List<String> learnMethods) {
     public String toString() {
-        return name + " " + type + " " + PP + " " + power + " " + accuracy + " " + learnMethod;
+        return name + " " + type + " " + PP + " " + power + " " + accuracy + " " + learnMethods;
     }
 }
