56e3f2acbfe9c0f85711eee78894661dab9b20bd
Pirate <Timnich2001@Gmail.com>
2022-03-19 14:07:41 -0400

    Refactors move

--- src/main/java/edu/bsu/cs222/model/Move.java
+++ src/main/java/edu/bsu/cs222/model/Move.java
@@ -2,7 +2,7 @@ package edu.bsu.cs222.model;
 
 import java.util.List;
 
-public record Move(String name, String type, String PP, String power, String accuracy, List<String> learnMethod, List<Integer> levelLearned) {
+public record Move(String name, String type, String PP, String power, String accuracy, String learnMethod) {
     public String toString() {
         return name + " " + type + " " + PP + " " + power + " " + accuracy + " " + learnMethod;
     }
