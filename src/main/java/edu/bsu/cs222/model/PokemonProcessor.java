1e74c9c4eb170e81773ff5048bc53fc2afb03562
pulian <paullian84@gmail.com>
2022-03-20 22:48:02 -0400

    delete some on my orginal work because it doest work
    i delete some

--- src/main/java/edu/bsu/cs222/model/PokemonProcessor.java
+++ src/main/java/edu/bsu/cs222/model/PokemonProcessor.java
@@ -1,14 +1,21 @@
 package edu.bsu.cs222.model;
 
 public class PokemonProcessor {
+    private URLProcessor Url = new URLProcessor();
+    private PokemonBuilder pokemonBuilder = new PokemonProcessor();
+    private PokemonVerifier pokemonVerifier = new PokemonVerifier();
+    private InputStreamConverter inputStreamConverter = new InputSteamConverter();
 
     public PokemonProcessor() {
+        
 
     }
 
     // TODO: Starting with this method, use the URLProcessor class, the parsers, and the data stored in Pokemon
     //  To return information on a given Pokemon to the main window. For now, it will simply throw a RuntimeException.
     public Pokemon process(String nameOfPokemon, String nameOfGame) throws RuntimeException {
+        String inputStreamString = Url.process("https://pokeapi.co/api/v2/pokemon/" + nameOfPokemon);
+
         throw new RuntimeException();
     }
 }
