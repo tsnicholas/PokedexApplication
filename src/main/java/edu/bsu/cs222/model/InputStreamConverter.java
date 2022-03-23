394a5934fced8b6529d88b71928659241dc260e4
Pirate <Timnich2001@Gmail.com>
2022-03-22 20:14:40 -0400

    Delete test method in InputStreamConverter

--- src/main/java/edu/bsu/cs222/model/InputStreamConverter.java
+++ src/main/java/edu/bsu/cs222/model/InputStreamConverter.java
@@ -8,14 +8,6 @@ import java.util.Scanner;
 public class InputStreamConverter {
     private final JsonParser jsonParser = new JsonParser();
 
-    // This version of the method is specifically for testing
-    public Object inputStreamToJsonObject(String name) {
-        InputStream testStreamData = Thread.currentThread().getContextClassLoader().getResourceAsStream(name + ".json");
-        assert testStreamData != null;
-        Scanner scanner = new Scanner(testStreamData);
-        return jsonParser.parseJson(scanner.nextLine());
-    }
-
     public Object inputStreamToJsonObject(InputStream stream) {
         assert stream != null;
         Scanner scanner = new Scanner(stream);
