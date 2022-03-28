e6c478e9d8b14ea3e403661e83ec10324212fe63
Kameron Rogers <kameron.rogers.2000@gmail.com>
2022-03-27 22:12:20 -0400

    Refactor tests so that there is only one assert per test.
    Replaced tests which had multiple calls to Assertions.assertEquals with
    parameterized tests.
    
    Added testImplementation into build.gradle so that parameterized tests
    could be used.

--- src/test/java/edu/bsu/cs222/model/PokedexBuilderTest.java
+++ src/test/java/edu/bsu/cs222/model/PokedexBuilderTest.java
@@ -1,7 +1,8 @@
 package edu.bsu.cs222.model;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 
 public class PokedexBuilderTest {
@@ -9,11 +10,11 @@ public class PokedexBuilderTest {
     private final PokedexFactory gameBuilder = new PokedexFactory();
     private final Object yellowJsonDocument = resourceConverter.inputStreamToJsonObject(Thread.currentThread().getContextClassLoader().getResourceAsStream("yellow.json"));
 
-    @Test
-    public void createGameTest() {
+    @ParameterizedTest
+    @CsvSource({"bulbasaur, 0", "mew, 150"})
+    public void createGameTest(String pokemonName, int pokemonIndex) {
         Pokedex game = gameBuilder.createPokedex(yellowJsonDocument);
-        Assertions.assertEquals("bulbasaur", game.getPokemonNames().get(0));
-        Assertions.assertEquals("mew", game.getPokemonNames().get(150));
+        Assertions.assertEquals(pokemonName, game.getPokemonNames().get(pokemonIndex));
     }
 
 }
