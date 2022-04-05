798db12793b9b8a5627c6fe2d632878df884f399
Kameron Rogers <karogers3@bsu.edu>
2022-04-05 06:00:10 -0400

    Refactor data structures to adhere to Builder design pattern.
    VersionGroupBuilder was removed as it was redundant. Added attributes to
    Version so that it can reference its generation and version group.

--- src/main/java/edu/bsu/cs222/model/Generation.java
+++ src/main/java/edu/bsu/cs222/model/Generation.java
@@ -3,22 +3,48 @@ package edu.bsu.cs222.model;
 import java.util.List;
 
 public class Generation {
-    private final String name;
-    private final int id;
+
+    public static Builder withName(String name) {
+        return new Builder(name);
+    }
+
+    public static final class Builder {
+        private final String generationName;
+        private Integer generationID;
+        private List<VersionGroup> versionGroups;
+
+        public Builder(String name) {
+            this.generationName = name;
+        }
+
+        public Builder andID(Integer id) {
+            this.generationID = id;
+            return this;
+        }
+
+        public Generation andVersionGroups(List<VersionGroup> versionGroups) {
+            this.versionGroups = versionGroups;
+            return new Generation(this);
+        }
+    }
+
+
+    private final String generationName;
+    private final Integer generationID;
     private final List<VersionGroup> versionGroups;
 
-    public Generation(String name, int id, List<VersionGroup> versionGroups) {
-        this.name = name;
-        this.id = id;
-        this.versionGroups = versionGroups;
+    public Generation(Builder builder) {
+        this.generationName = builder.generationName;
+        this.generationID = builder.generationID;
+        this.versionGroups = builder.versionGroups;
     }
 
-    public String getName() {
-        return name;
+    public String getGenerationName() {
+        return generationName;
     }
 
-    public int getId() {
-        return id;
+    public Integer getGenerationID() {
+        return generationID;
     }
 
     public List<VersionGroup> getVersionGroups() {
--- src/main/java/edu/bsu/cs222/model/Version.java
+++ src/main/java/edu/bsu/cs222/model/Version.java
@@ -1,13 +1,50 @@
 package edu.bsu.cs222.model;
 
 public class Version {
-    private final String name;
-    private final String versionGroup;
 
-    //TODO: get generation info
+    public static Builder withName(String name) {
+        return new Builder(name);
+    }
 
-    public Version(String name, String verisonGroup) {
-        this.name = name;
-        this.versionGroup = verisonGroup;
+    public static final class Builder {
+        private final String versionName;
+        private VersionGroup versionGroup;
+        private Generation generation;
+
+        public Builder(String versionName) {
+            this.versionName = versionName;
+        }
+
+        public Builder andVersionGroup(VersionGroup versionGroup) {
+            this.versionGroup = versionGroup;
+            return this;
+        }
+
+        public Version andGeneration(Generation generation) {
+            this.generation = generation;
+            return new Version(this);
+        }
+    }
+
+    private final String versionName;
+    private final VersionGroup versionGroup;
+    private final Generation generation;
+
+    public Version(Builder builder) {
+        this.versionName = builder.versionName;
+        this.versionGroup = builder.versionGroup;
+        this.generation = builder.generation;
+    }
+
+    public String getVersionName() {
+        return versionName;
+    }
+
+    public VersionGroup getVersionGroup() {
+        return versionGroup;
+    }
+
+    public Generation getGeneration() {
+        return generation;
     }
 }
