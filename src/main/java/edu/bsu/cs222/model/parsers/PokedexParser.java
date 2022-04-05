1a6859201b8fa5a3dd607564232ceec6de074909
Kameron Rogers <karogers3@bsu.edu>
2022-04-04 21:19:55 -0400

    Refactor pokedexparser to return a List<String> and clean up code.
    JSONParser's JsonArrayToString method was not needed.
    
    Returns List<String> instead of Pokedex so that it can be tested
    independently.

--- src/main/java/edu/bsu/cs222/model/parsers/PokedexParser.java
+++ src/main/java/edu/bsu/cs222/model/parsers/PokedexParser.java
@@ -1,17 +1,12 @@
 package edu.bsu.cs222.model.parsers;
 
 import com.jayway.jsonpath.JsonPath;
-import edu.bsu.cs222.model.Pokedex;
-import net.minidev.json.JSONArray;
 
 import java.util.List;
 
 public class PokedexParser {
-    private final JsonParser jsonParser = new JsonParser();
 
-    public Pokedex parsePokemonNames(Object nationalDexJsonObject) {
-        JSONArray pokemonNamesArray = JsonPath.read(nationalDexJsonObject, "$.pokemon_entries..pokemon_species.name");
-        List<String> pokemons = jsonParser.jsonArrayToStringList(pokemonNamesArray);
-        return new Pokedex(pokemons);
+    public List<String> parseForPokemonNames(Object pokedexJsonObject) {
+        return JsonPath.read(pokedexJsonObject, "$.pokemon_entries..pokemon_species.name");
     }
 }
