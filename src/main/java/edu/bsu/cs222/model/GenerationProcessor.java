d4374c3b4c56f158459c01f7632a91049d0962bb
Kameron Rogers <karogers3@bsu.edu>
2022-04-05 06:06:50 -0400

    Restructure how list of all versions is created and retrieved.
    Instead of getting a list of generations, and interacting with that, we can
    just fetch a list of versions, which is created by first getting a list of
    generations and then version groups.
    
    Parsers were restructured so that they no longer contain any URL calls
    or connection to the internet. All connection to internet will be handled
    in Fetcher or Processor classes.
    
    Slight modifications were made to view, just to account for the change from
    a list of generations to a list of versions.
    
    All Parers are now thoroughly tested.

--- src/main/java/edu/bsu/cs222/model/GenerationProcessor.java
+++ src/main/java/edu/bsu/cs222/model/GenerationProcessor.java
@@ -6,15 +6,15 @@ import java.util.HashMap;
 import java.util.List;
 
 public class GenerationProcessor {
-    public List<Generation> createGenerationList() {
-        GenerationParser generationParser = new GenerationParser();
-        return generationParser.parseForGenerations();
-    }
+//    public List<Generation> createGenerationList() {
+//        GenerationParser generationParser = new GenerationParser();
+//        return generationParser.parseForGenerations();
+//    }
 
     public HashMap<String, Integer> getGenerationIDs(List<Generation> generations) {
         HashMap<String, Integer> generationIDMap = new HashMap<>();
         for (Generation generation : generations) {
-            generationIDMap.put(generation.getName(), generation.getId());
+            generationIDMap.put(generation.getGenerationName(), generation.getGenerationID());
         }
         return generationIDMap;
     }
---
+++ src/test/java/edu/bsu/cs222/model/parsers/VersionGroupParserTest.java
@@ -0,0 +1,29 @@
+package edu.bsu.cs222.model.parsers;
+
+import edu.bsu.cs222.model.InputStreamConverter;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.List;
+
+public class VersionGroupParserTest {
+    private final VersionGroupParser versionGroupParser = new VersionGroupParser();
+    private final InputStreamConverter resourceConverter = new InputStreamConverter();
+    private final Object xyVersionGroupJsonDocument = resourceConverter.inputStreamToJsonObject(Thread.currentThread()
+            .getContextClassLoader().getResourceAsStream("x-y.json"));
+
+    @Test
+    public void testParseForName_xy() {
+        String actual = versionGroupParser.parseForName(xyVersionGroupJsonDocument);
+        Assertions.assertEquals("x-y", actual);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"0, x", "1, y"})
+    public void testParseForVersionNames_xAndY(int index, String expected) {
+        List<String> actual = versionGroupParser.parseForVersionNames(xyVersionGroupJsonDocument);
+        Assertions.assertEquals(expected, actual.get(index));
+    }
+}
