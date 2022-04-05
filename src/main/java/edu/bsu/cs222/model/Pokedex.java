7d7dbe33921ea535b546c4eaf9fd6625f736272f
Kameron Rogers <karogers3@bsu.edu>
2022-04-04 21:17:22 -0400

    Refactor pokedex classes and add NationalPokedexFetcher

--- src/main/java/edu/bsu/cs222/model/Pokedex.java
+++ src/main/java/edu/bsu/cs222/model/Pokedex.java
@@ -3,13 +3,13 @@ package edu.bsu.cs222.model;
 import java.util.List;
 
 public class Pokedex {
-    private final List<String> pokemonName;
+    private final List<String> pokemonNames;
 
     public Pokedex(List<String> pokemonList) {
-        this.pokemonName = pokemonList;
+        this.pokemonNames = pokemonList;
     }
 
-    public List<String> getPokemonNames() {
-        return pokemonName;
+    public boolean containsPokemon(String pokemonName) {
+        return this.pokemonNames.contains(pokemonName);
     }
 }
