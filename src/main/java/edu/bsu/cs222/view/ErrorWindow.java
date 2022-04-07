f7bc70bbc08d439562bf1384cbe761ade0861f12
Pirate <Timnich2001@Gmail.com>
2022-04-07 13:53:39 -0400

    Add icon for error windows

--- src/main/java/edu/bsu/cs222/view/ErrorWindow.java
+++ src/main/java/edu/bsu/cs222/view/ErrorWindow.java
@@ -1,13 +1,16 @@
 package edu.bsu.cs222.view;
 
 import javafx.scene.control.Alert;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
 
 public class ErrorWindow {
-
     private final Alert error;
 
     public ErrorWindow(String errorReason) {
         error = new Alert(Alert.AlertType.ERROR, errorReason);
+        Stage stage = (Stage) error.getDialogPane().getScene().getWindow();
+        stage.getIcons().add(new Image("Error Icon.jpg"));
     }
 
     public void display() {
