e284f7997e301b9b18ae7b10ab78ef43553f2dfc
Kameron Rogers <karogers3@bsu.edu>
2022-04-07 20:48:40 -0400

    Refactor MoveParserTest so that it does not connect to the internet

--- src/test/java/edu/bsu/cs222/model/parsers/MoveParserTest.java
+++ src/test/java/edu/bsu/cs222/model/parsers/MoveParserTest.java
@@ -2,7 +2,6 @@ package edu.bsu.cs222.model.parsers;
 
 import edu.bsu.cs222.model.*;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -13,28 +12,29 @@ import java.util.List;
 public class MoveParserTest extends TestResourceConverter {
     private final Object tackle = convertFileNameToObject("tackle.json");
     private final MoveParser moveParser = new MoveParser();
-    private Version white;
-    private Version moon;
+    private final HashMap<String, Integer> versionGroupMap = makeVersionGroupMap();
 
-    @BeforeEach
-    public void setUpVersion() {
-        VersionListFetcher versionListFetcher = new VersionListFetcher();
-        List<Version> versions = versionListFetcher.getListOfAllVersions();
-        white = versions.get(19);
-        moon = versions.get(26);
+    private HashMap<String, Integer> makeVersionGroupMap() {
+        HashMap<String, Integer> versionGroupMap = new HashMap<>();
+        versionGroupMap.put("black-white", 11);
+        versionGroupMap.put("sun-moon", 17);
+        return versionGroupMap;
     }
 
     @Test
     public void testParseForMoves_tacklePowerInWhiteVersion() {
+        VersionGroup blackAndWhite = VersionGroup.withName("black-white").andID(11).andVersionNames(null);
+        Version white = Version.withName(null).andVersionGroup(blackAndWhite).andVersionGroupMap(versionGroupMap);
         Move actual = moveParser.parseForMove(tackle, List.of("Lv 1"), white);
-        Move expected = Move.withName(null).andType(null).andPP(null).andPower("50")
-                .andAccuracy(null).andLearnMethods(null);
+        Move expected = Move.withName(null).andPower("50").andLearnMethods(null);
         Assertions.assertEquals(expected.getPower(), actual.getPower());
     }
 
     @ParameterizedTest
     @CsvSource({"name", "type", "pp", "power", "accuracy"})
     public void testParseForMoves_tackleInMoonVersion(String key) {
+        VersionGroup sunAndMoon = VersionGroup.withName("sun-moon").andID(17).andVersionNames(null);
+        Version moon = Version.withName(null).andVersionGroup(sunAndMoon).andVersionGroupMap(versionGroupMap);
         Move actualMove = moveParser.parseForMove(tackle, List.of("Lv 1"), moon);
         Move expectedMove = Move.withName("tackle").andType("normal").andPP("35").andPower("40")
                 .andAccuracy("100").andLearnMethods(null);
