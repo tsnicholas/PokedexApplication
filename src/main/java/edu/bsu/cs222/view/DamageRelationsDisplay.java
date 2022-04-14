35decfb73b3e511fdb4d0119caefb5a1ea07fd2b
Tim Nicholas <tsnicholas@bsu.edu>
2022-04-14 19:51:27 -0400

    Refactor DamageRelationDisplay to make damage relation names bold

--- src/main/java/edu/bsu/cs222/view/DamageRelationsDisplay.java
+++ src/main/java/edu/bsu/cs222/view/DamageRelationsDisplay.java
@@ -2,13 +2,18 @@ package edu.bsu.cs222.view;
 
 import edu.bsu.cs222.model.Pokemon;
 import javafx.scene.Parent;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 
 import java.util.List;
 
 public class DamageRelationsDisplay implements MenuDisplay {
+    private static final String FONT_NAME = "Times New Roman";
+    private static final int FONT_SIZE = 25;
+
     public Parent display(Pokemon pokemon) {
         VBox rows = new VBox();
         rows.getChildren().addAll(
@@ -19,20 +24,34 @@ public class DamageRelationsDisplay implements MenuDisplay {
         return rows;
     }
 
-    private Text createDamageRelationRow(String damageRelation, List<String> damageRelationList) {
+    private Parent createDamageRelationRow(String damageRelation, List<String> damageRelationList) {
+        HBox damageRelationRow = new HBox();
+        damageRelationRow.setSpacing(5);
+        damageRelationRow.getChildren().addAll(
+                createDamageRelationName(damageRelation),
+                createDamageRelationData(damageRelationList)
+        );
+        return damageRelationRow;
+    }
+
+    private Text createDamageRelationName(String damageRelation) {
+        Text text = new Text(damageRelation + ": ");
+        text.setFont(Font.font(FONT_NAME, FontWeight.BOLD, FONT_SIZE));
+        return text;
+    }
+
+    private Text createDamageRelationData(List<String> damageRelationList) {
         StringBuilder output = new StringBuilder();
-        output.append(damageRelation);
-        output.append(": ");
         for(String listValue: damageRelationList) {
             output.append(listValue);
             output.append(" ");
         }
-        return convertStringToText(output.toString());
+        return createText(output.toString());
     }
 
-    private Text convertStringToText(String input) {
-        Text text = new Text(input);
-        text.setFont(Font.font("Times New Roman", 25));
+    private Text createText(String type) {
+        Text text = new Text(type);
+        text.setFont(Font.font(FONT_NAME, FONT_SIZE));
         return text;
     }
 
