7c8eda46600148addc583497bb5de498752813d3
Kameron Rogers <karogers3@bsu.edu>
2022-04-22 18:11:37 -0400

    Add ControlsFX dependency
    ControlsFX's TextFields class can be used to generate an autocomplete list when users
    are searching for a pokemon.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module Pokedex.main {
     requires javafx.controls;
     requires json.path;
     requires json.smart;
+    requires org.controlsfx.controls;
     exports edu.bsu.cs222.model;
     exports edu.bsu.cs222.model.parsers;
     exports edu.bsu.cs222.view;
