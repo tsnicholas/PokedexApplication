4a68d0d8b427684e3a6a7c6b6d05d98e05b19326
Kameron Rogers <karogers3@bsu.edu>
2022-04-24 00:54:11 -0400

    Remove BeforeEach method in NationalPokedexTest to increase test speed
    BeforeEach method made this test significantly slower, and it was unnecessary as the
    nationalPokedex object was not being altered by any methods.

--- src/test/java/edu/bsu/cs222/model/NationalPokedexTest.java
+++ src/test/java/edu/bsu/cs222/model/NationalPokedexTest.java
@@ -1,20 +1,14 @@
 package edu.bsu.cs222.model;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class NationalPokedexTest {
     private final TestURLProcessor testURLProcessor = new TestURLProcessor();
-    private NationalPokedex nationalPokedex;
-
-    @BeforeEach
-    public void setup() {
-        NationalPokedexFactory nationalPokedexFactory = new NationalPokedexFactory(testURLProcessor);
-        this.nationalPokedex = nationalPokedexFactory.createNationalPokedex();
-    }
+    private final NationalPokedexFactory nationalPokedexFactory = new NationalPokedexFactory(testURLProcessor);
+    private final NationalPokedex nationalPokedex = nationalPokedexFactory.createNationalPokedex();
 
     @ParameterizedTest
     @CsvSource({"bulbasaur", "calyrex"})
